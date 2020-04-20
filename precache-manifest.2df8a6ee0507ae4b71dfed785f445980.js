8a16912ab1e54361873ed9ff3e56db1aa8def5b6
Cl3r1k <Cl3r1k2@gmail.com>
2020-04-20 10:38:03 +0700

    Updates

---
+++ precache-manifest.2df8a6ee0507ae4b71dfed785f445980.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53abb2f43fe2a2042c99e154b48ed0d7",
+    "url": "/react-datagrid/index.html"
+  },
+  {
+    "revision": "b63f19ee53785c1e1a59",
+    "url": "/react-datagrid/static/css/main.6d4c779b.chunk.css"
+  },
+  {
+    "revision": "30cbf12d6bc91eb4fee3",
+    "url": "/react-datagrid/static/js/2.cc335d86.chunk.js"
+  },
+  {
+    "revision": "7d9fa850aa2ff6b3a58494c222b6b450",
+    "url": "/react-datagrid/static/js/2.cc335d86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b63f19ee53785c1e1a59",
+    "url": "/react-datagrid/static/js/main.516a243b.chunk.js"
+  },
+  {
+    "revision": "1ec6fea23bb84c0c8f80",
+    "url": "/react-datagrid/static/js/runtime-main.8f06cb41.js"
+  }
+]);
